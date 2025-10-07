a2c13521369b1d3ca838dbcb70b0be2880ad92fd
SandalaDev <abesandala@outlook.com>
2025-10-07 03:04:49 +0200

    migration file

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251005_234018 from './20251005_234018';
+import * as migration_20251007_010301 from './20251007_010301';
 
 export const migrations = [
   {
     up: migration_20251005_234018.up,
     down: migration_20251005_234018.down,
-    name: '20251005_234018'
+    name: '20251005_234018',
+  },
+  {
+    up: migration_20251007_010301.up,
+    down: migration_20251007_010301.down,
+    name: '20251007_010301'
   },
 ];
