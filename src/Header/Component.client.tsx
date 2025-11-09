9a7e82d606e387eeb1c2ac3d3c1bea01bc6e0810
SandalaDev <abesandala@outlook.com>
2025-11-09 03:27:13 +0200

    pre menu

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -17,8 +17,8 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   const pathname = usePathname()
 
   return (
-    <header className="container relative z-20  header-glass mt-[-0.25rem] shadow-card">
-      <div className="py-8 flex justify-between">
+    <header className="container relative z-20  header-glass mt-[-0.25rem] shadow-card ">
+      <div className="py-8 flex justify-between text-red-600">
         <Link href="/">
           <Logo loading="eager" priority="high" className="" />
         </Link>
